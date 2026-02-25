c3b579e837563621ef5152ce7d008f29da6976c4
Mafufuyu <datgorilla12124@gmail.com>
2026-02-25 19:34:16 +0700

    Add project files.

---
+++ DriveLens/DriveLens.cpp
@@ -0,0 +1,18 @@
+﻿// DriveLens.cpp : Defines the entry point for the application.
+//
+
+#include "DriveLens.h"
+
+using namespace std;
+
+int main()
+{
+	// Verify OpenCV
+	cout << "[DriveLens] OpenCV version: " << CV_VERSION << endl;
+
+	// Verify cpr (HTTP client)
+	cpr::Response r = cpr::Get(cpr::Url{"https://httpbin.org/get"});
+	cout << "[DriveLens] HTTP status : " << r.status_code << endl;
+
+	return 0;
+}
